d924c162e4a1604958727381fa8eeadf3d143eb3
mvrcxs25 <45280675+mvrcxs25@users.noreply.github.com>
2019-11-26 09:56:02 -0600

    Add files via upload

--- input.s
+++ input.s
@@ -6,8 +6,8 @@
 ; a security system.
 		
 		
-count		equ 	2						; ( # of digits)  leave at 2 for testing purposes
-codeG		dcb		0x02, 0x05				; security code that needs to be matched to the testing digits.
+count		equ 	3					; ( # of digits)  leave at 2 for testing purposes
+codeG		dcb		0x06, 0x02, 0x05			; security code that needs to be matched to the testing digits.
 counter		RN		r12		
 			export __main
 __main 		 proc
@@ -46,13 +46,13 @@ moveAgain
 			
 			
 ; input P4 set up		
-			ldr r0, =0x40004C21			;p4 address 0x21
+			ldr r0, =0x40004C21
+			mov r1, #0x0F
+			strb r1, [r0, #4] 				; P0-3 Outputs, P4-7 Inputs
+			mov r1, #0xF0
+			strb r1, [r0, #6] 				; enables resistors for P4-7
 			mov r1, #0x00
-			strb r1, [r0, #4]			;p4 all are input pins. 0x00 DIR
-			strb r1, [r0, #2]			; P4REN for all pins
-			mov r1, #0xFF				; 0-7 pins 
-			strb r1, [r0, #6]			; P4 pull up resistor set, maybe?
-			
+			strb r1, [r0, #2]				; Start at pull down( 00)
 
 ; setting output as P2.0 ( built in LED)
 			mov r2, #0x01				; 0x01 pin 0 on.
@@ -65,51 +65,130 @@ moveAgain
 ; PIR setup
 ; 1st pin is 5v, 2nd is output ( on or off), 3rd pin is ground
 ; it will operate as an input pin
+
+
 ; PIR is only to be activated once keycode is correct.
 
 			ldr r4, =0x40004C41			;P6 address
-			mov r1, #0x00
+			mov r1, #0x01
 			strb r1,[r4, #4]			; DIR input pins
 			strb r1,[r4, #2]			; pull up resistor set.
 			mov r1, #0x01
 			strb r1, [r4, #6]			; P6REN for pin 1.
 			
-			ldr r5, =0x20000000
-			mov r12, #0x00
 
 			
-again		
-			mov r2, #0xFF
-			cmp counter, #count			; once count is reach will branch to test statement
-			beq Test
-			ldrb r2, [r0]				; whatever is stored at 40004C21 (input pins P4) is now at r2			
-			and r2, #0xFF				; keep all pins
+			ldr r5, =0x20000000
+			mov r12, #0x00
 			
-			cmp r2, #0x01
+			
+			
+			
+again		
+
+			cmp r12, #count					; once count is reach will branch to test statement
+			beq.w test
+			
+begin
+			mov r1, #0x01
+			strb r1, [r3, #2]
+			
+			mov r1, #0x01
+			strb r1, [r0, #2]
+			ldrb r2, [r0]
+			and r2, #0xF0
+			cmp r2, #0x00
+			bne Row1					; Will move as long as there is an output valve 
+			
+			
+			mov r1, #0x02
+			strb r1, [r0, #2]
+			ldrb r2, [r0]
+			and r2, #0xF0
+			cmp r2, #0x00
+			bne Row2
+			
+			
+			mov r1, #0x04
+			strb r1, [r0, #2]
+			ldrb r2, [r0]
+			and r2, #0xF0
+			cmp r2, #0x00
+			bne Row3
+			
+			
+			mov r1, #0x08
+			strb r1, [r0, #2]
+			ldrb r2, [r0]
+			and r2, #0xF0
+			cmp r2, #0x00
+			bne Row4
+		
+			b begin
+			
+
+Row1
+			add r2, #0x01				; want our outputs P4-7, test 0x11
+			b Delay1
+
+Row2
+			add r2, #0x02				; want our outputs P4-7, test 0x11
+			b Delay1
+
+Row3
+			add r2, #0x04				; want our outputs P4-7, test 0x11
+			b Delay1
+
+Row4
+			add r2, #0x08 				; want our outputs P4-7, test 0x11
+			b Delay1
+
+
+Delay1
+			mov r1, #0x00
+			strb r1, [r3, #2]
+			
+			mov r1, #1000
+outer1
+			mov r8, #250
+			
+inner1
+			subs r8, #1
+			bne inner1
+			subs r1,#1
+			bne outer1
+			b inputV
+			
+			
+		
+inputV										;<-- bring in r2 full values
+		
+
+			cmp r2, #0x28
 			beq CH0			
-			cmp r2, #0x02
+			cmp r2, #0x11
 			beq CH1
-			cmp r2, #0x04
+			cmp r2, #0x21
 			beq CH2
-			cmp r2, #0x08
+			cmp r2, #0x41
 			beq CH3			
-			cmp r2, #0x10
+			cmp r2, #0x12
 			beq CH4
-			cmp r2, #0x20
+			cmp r2, #0x22
 			beq CH5
-			cmp r2, #0x40
+			cmp r2, #0x42
 			beq CH6			
-			cmp r2, #0x80
+			cmp r2, #0x14
 			beq CH7
-			cmp r2, #0x50
+			cmp r2, #0x24
 			beq CH8
-			cmp r2, #0xA0
+			cmp r2, #0x44
 			beq CH9
 
 			
 			mov r1, #0x00
 			strb r1, [r3, #2]
-			b again
+			b begin
 			
 ; Channels below have values from 0-9, and will store a Hex number 
 ; in memory depending on which channel is entered.
@@ -117,7 +196,8 @@ again
 CH0			
 			mov r1, #0x00
 			strb r1,[r5,counter]
-			add counter, #1			
+			add counter, #1	
+			
 			b delay	
 CH1			
 			mov r1, #0x01
@@ -168,10 +248,8 @@ CH9
 ; Test is when counter has reached the count number and will test the 
 ; security code entered vs the one given
 
-Test
+test
 			mov counter, #0
-			mov r1, #0x01
-
 			
 			;ldr r5, =0x20000000
 			;ldr r7, =0x20001000
@@ -212,7 +290,7 @@ exit3
 ; security system code matches the one given.			
 lightUp										
 			mov r1, #0x01			
-			strb r1,[ r3, #2]
+			strb r1,[ r4, #2]
 											; will need to compare to leave the statment and 
 											; possibly move to PIR detection system?
 			b lightUp
@@ -222,9 +300,9 @@ lightUp
 ; only used when an input is sensed. 
 			
 delay
-			mov r1, #10000
+			mov r1, #1000
 outer
-			mov r2, #250
+			mov r2, #500
 			
 inner
 			subs r2, #1
